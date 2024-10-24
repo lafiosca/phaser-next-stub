fc355e2be720799aada8e093ddbe1fbd2ea10d61
Joe Lafiosca <joe@lafiosca.com>
2024-10-23 23:49:29 -0400

    phaser stub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import styles from "./page.module.css";
+import AppWithoutSSR from './AppWithoutSSR';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <p>nothin</p>
+      <AppWithoutSSR />
     </div>
   );
 }
